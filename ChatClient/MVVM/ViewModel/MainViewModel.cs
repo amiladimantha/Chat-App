d2ed5915581e509f791617adddfc89965d4b3c78
Amila Dimantha <amiladimantha123@gmail.com>
2023-03-08 12:49:40 +0530

    initial push

---
+++ ChatClient/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ChatClient.MVVM.Core;
+using ChatClient.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCmd ConnectToServerCommand { get; set; }
+
+        public Server _server;
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCmd(o => _server.ConnectToServer());
+        }
+    }
+}
---
+++ ChatClient/Net/Server.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.Net
+{
+    internal class Server
+    {
+        TcpClient _client;
+        public Server() 
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnectToServer()
+        {
+            if (!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+            }
+        }
+
+    }
+}
